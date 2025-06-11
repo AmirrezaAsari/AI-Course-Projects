655841feaaab03e9436ce3356310c37653a5e266
Amirreza Asari <104520102+AmirrezaAsari@users.noreply.github.com>
2025-06-11 20:05:38 +0330

    update README

--- local-search-project/SA.py
+++ local-search-project/SA.py
@@ -1,5 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
 def objective(x, y):
     value = 3*(1-x)**2*np.exp(-x**2 - (y+1)**2) - 10*(x/5 - x**3 - y**5)*np.exp(-x**2 - y**2) -1/3*np.exp(-(x+1)**2 - y**2)
